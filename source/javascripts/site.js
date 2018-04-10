b96dfbd2cd779a9bc575ffb576157bd2de4a0051
Galalafm <gala.pill@gmail.com>
2018-04-11 00:00:47 +0200

    add html structure & style

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,7 @@
-// This is where it all goes :)
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    console.log(scroll);
+    if (scroll >= 50) {
+        $(".header").addClass("header-scrolled");
+    }
+});
